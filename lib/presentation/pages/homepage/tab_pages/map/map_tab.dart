19f818cc2994c3304270d64ebddc432229060c16
personal <anasboyka98@gmail.com>
2022-12-17 06:53:42 +0800

    setup google map

--- lib/presentation/pages/homepage/tab_pages/map/map_tab.dart
+++ lib/presentation/pages/homepage/tab_pages/map/map_tab.dart
@@ -1,6 +1,7 @@
 import 'package:carrypill_rider/constant/constant_color.dart';
 import 'package:carrypill_rider/constant/constant_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapTab extends StatefulWidget {
   const MapTab({Key? key}) : super(key: key);
@@ -10,28 +11,41 @@ class MapTab extends StatefulWidget {
 }
 
 class _MapTabState extends State<MapTab> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(3.2077, 101.6899);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kcBgHome,
-      appBar: AppBar(
-        title: Text(
-          'Map',
-          style: kwtextStyleRD(
-            fs: 20,
-            fw: kfbold,
-          ),
-        ),
-        elevation: 0,
-      ),
-
-      // body: GoogleMap(
-      //   onMapCreated: _onMapCreated,
-      //   initialCameraPosition: CameraPosition(
-      //     target: _center,
-      //     zoom: 11.0,
+      // appBar: AppBar(
+      //   title: Text(
+      //     'Map',
+      //     style: kwtextStyleRD(
+      //       fs: 20,
+      //       fw: kfbold,
+      //     ),
       //   ),
+      //   elevation: 0,
       // ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 20.0,
+        ),
+      ),
     );
   }
 }
