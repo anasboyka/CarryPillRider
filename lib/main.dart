795de0ab9876f4c97da99ca3e107d0256f9c58f1
personal <anasboyka98@gmail.com>
2023-01-08 05:12:19 +0800

    latest 08012923

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ import 'package:provider/provider.dart';
 import 'data/datarepositories/map_repo/location_repo.dart';
 
 void main() async {
-  //al punya testing branch skali lagi
   WidgetsFlutterBinding.ensureInitialized();
   await Spreferences.init();
 
